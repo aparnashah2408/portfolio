d4f955749e8a6e15a6f4d13f564b746eb27d6197
Aparna Shah <72094753+aparnashah2408@users.noreply.github.com>
2021-08-29 12:26:54 +0530

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -87,7 +87,7 @@ function Title(props) {
 
 
         <h1 className="text-center text-white name" >
-      Hi I am, <span className="text-warning font1">Distorted Ass.</span> 
+      Hi I am, <span className="text-warning font1">Aparna Shah.</span> 
       <br/>
       I am a <span className="text-warning font1">freelancer</span> from India.
     </h1>
